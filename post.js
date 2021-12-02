0197f31a134c301f466f47a0615174847de7cd22
Noe Desprez <noe.desprez@etu.umontpellier.fr>
2021-12-02 21:21:11 +0100

    correction

--- post.js
+++ post.js
@@ -2,8 +2,8 @@ function post(pseudo, message) {
     let fil=document.getElementById("fil");
     let newP=document.createElement('p');
 
-    newP.innerHTML = pseudo + " " + message;
-    fil.append(newP)
+    newP.innerText = pseudo + " " + message;
+    fil.append(newP);
     //newP.textContent='>>'+pseudo+' : '+message;
     //fil.prepend(newP);
 }
